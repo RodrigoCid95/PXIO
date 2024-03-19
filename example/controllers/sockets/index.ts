32bf6432b8a7b679e299f6b6d0de1b1b5e55099f
Rodrigo Cid <rodrigocid95@gmail.com>
2024-03-18 20:36:30 -0600

    Update example

--- example/controllers/sockets/index.ts
+++ example/controllers/sockets/index.ts
@@ -1,21 +1,23 @@
+declare const Namespace: PXIO.NamespaceDecorator
 declare const Model: PXIO.ModelDecorator
 declare const On: PXIOSockets.OnDecorator
 
+@Namespace('chat')
 export class IndexController {
   @Model('MiModelo') model: Models<'MiModelo'>
   constructor(private io: PXIOSockets.IO) { }
   @On('connect')
-  public join(socket: PXIOSockets.Socket) {
+  public join(socket: PXIOSockets.Socket): void {
     const { user_name, full_name } = (socket.handshake as any).session.user
     socket.broadcast.emit('join', { user_name, full_name })
   }
   @On('message')
-  public message(message: string, socket: PXIOSockets.Socket) {
+  public message(message: string, socket: PXIOSockets.Socket): void {
     const { user_name } = (socket.handshake as any).session.user
     this.io.emit('message', { user_name, message })
   }
   @On('disconnect')
-  public leave(_: string, socket: PXIOSockets.Socket) {
+  public leave(_: string, socket: PXIOSockets.Socket): void {
     const { user_name, full_name } = (socket.handshake as any).session.user
     socket.broadcast.emit('leave', { user_name, full_name })
   }
